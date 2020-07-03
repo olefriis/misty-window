fd8422ef5081ee02897bf0b485a63601a6c7b6a7
Ole Friis Østergaard <ole.ostergaard@microsoft.com>
2020-07-03 08:46:27 +0200

    Initial Commit

---
+++ Raindrops and Mist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Raindrops and Mist
+//
+//  Created by Ole Friis Østergaard on 03/07/2020.
+//  Copyright © 2020 Retrofit Games. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
